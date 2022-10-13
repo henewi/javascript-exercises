252a4ba584dd1be134d33e3d3d261466179eb617
henewi <hpdunn2002@gmail.com>
2022-10-13 17:18:37 +0100

    ex4 failed again

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,10 @@
-function fn() {
+function removeFromArray() {
     var array = [1,2,3,4];
-    var popped = array.pop(3);
-    
+    var popped = array.pop(2);
+    return array;
 }
+
+removeFromArray();
 
 // Do not edit below this line
 module.exports = removeFromArray;
