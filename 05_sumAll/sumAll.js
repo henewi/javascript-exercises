60e07da61ef9cbbb46c9a352b0d7e13b6ee9334e
henewi <hpdunn2002@gmail.com>
2022-10-14 14:28:41 +0100

    ex5 wrong

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+// Take 2 numbers
+// Fill in the gaps
+// Add up the new array 
+// log the answer
 
-};
+const arr = Array.from({
+    length: 5 
+}, (v,k) => k + 1)
+
+console.log(arr)
+
+const sumAll = arr.reduce(add,0);
+    function add(accumulator, a) {
+        return accumulator + a;
+    }
+
+console.log(sumAll)
 
 // Do not edit below this line
 module.exports = sumAll;
